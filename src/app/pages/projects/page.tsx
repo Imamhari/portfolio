f94298369b3628a791de1aa3c4580688b4d64328
Hari <imamhari3001@gmail.com>
2025-02-14 05:38:42 +0700

    project page: ongoing

--- src/app/pages/projects/page.tsx
+++ src/app/pages/projects/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useState } from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import {
@@ -9,7 +11,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/app/components/ui/tooltip";
-import { Globe, Github } from 'lucide-react';
+import { MoveUpRight, Github } from 'lucide-react';
 import { JetBrains_Mono, Courier_Prime } from 'next/font/google';
 
 const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'], weight: ['400', '700'] });
@@ -21,10 +23,10 @@ const projectList = [
     category: "full-stack",
     title: "Laut Lestari",
     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, tempore ad fugiat facere perspiciatis voluptatem, laudantium temporibus nisi laborum odit repudiandae vitae exercitationem consequuntur veniam illo earum? Ad, voluptatem sapiente.",
-    stack: [{ name:"Next JS"}, { name:"Flask"}, { name:"Typescript"}, { name:"Python"}, { name:"Tailwind CSS"}, { name:"MySQL"}],
+    stack: [{ name:"Next JS,"}, { name:"Flask,"}, { name:"Typescript,"}, { name:"Python,"}, { name:"Tailwind CSS,"}, { name:"MySQL"}],
     image: "/public/assets/Laut-Lestari.png",
-    live:"",
-    github:"",
+    live:"https://lautlestari.netlify.app/",
+    github:"https://github.com/diikafirmansyah/FSSE-jan24_group-i_gfp_FE",
   },
   {
     num: "02",
@@ -33,18 +35,18 @@ const projectList = [
     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, tempore ad fugiat facere perspiciatis voluptatem, laudantium temporibus nisi laborum odit repudiandae vitae exercitationem consequuntur veniam illo earum? Ad, voluptatem sapiente.",
     stack: [{ name:"Next JS"}, { name:"Typescript"}, { name:"Tailwind CSS"}, { name:"ShadCN"}],
     image: "/public/assets/delish.png",
-    live:"",
-    github:"",
+    live:"https://frontend-delish-app.vercel.app/",
+    github:"https://github.com/tobiasagyasta/frontend-delish-app",
   },
   {
     num: "03",
     category: "front-end",
     title: "SahabatAjar.id",
     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, tempore ad fugiat facere perspiciatis voluptatem, laudantium temporibus nisi laborum odit repudiandae vitae exercitationem consequuntur veniam illo earum? Ad, voluptatem sapiente.",
-    stack: [{ name:"Next JS"}, { name:"Typescript"}, { name:"Tailwind CSS"}, { name:"ShadCN"}],
+    stack: [{ name:"Next JS,"}, { name:"Typescript"}, { name:"Tailwind CSS"}, { name:"ShadCN"}],
     image: "/public/assets/Sahabat-ajar.png",
-    live:"",
-    github:"",
+    live:"https://sahabatajar.vercel.app/",
+    github:"https://github.com/tobiasagyasta/lms-frontend",
   }
 ]
 
@@ -59,7 +61,47 @@ function Projects() {
                 {/* outline number */}
                 <div className={`${jetBrainsMono.className} text-8xl font-extrabold text-transparent text-outline`}>{project.num}</div>
                 {/* project category */}
-                <h2 className={`${courierPrime.className} text-[42px] font-bold leading-none group-hover:text-accent transition-all duration-500 capitalize`}>{project.category}</h2>
+                <h2 className={`${courierPrime.className} text-[30px] md:text-[42px] font-bold leading-none group-hover:text-accent transition-all duration-500 capitalize`}>{project.category}</h2>
+                {/* project description */}
+                <p className={`${courierPrime.className} text-[13px] text-justify text-gray-600 dark:text-gray-400 font-medium group-hover:text-accent  transition-all duration-500`}>{project.description}</p>
+                {/* project stack */}
+                <ul className="flex flex-wrap gap-2 mt-2">
+                  {project.stack.map((item, index) => {
+                    return (
+                      <li key={index} className={`${courierPrime.className} text-[15px] text-green-500`}>{item.name}</li>
+                    )
+                  })}
+                </ul>
+                {/* border */}
+                <div className="border border-orange-300"></div>
+                <div className="flex gap-2 mt-[2px]">
+                  {/* live project button */}
+                <Link href={project.live} target="_blank" rel="noopener noreferrer" className="" >
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[40px] h-[40px] dark:bg-gray-700 bg-gray-400 rounded-full flex justify-center  items-center group">
+                        <MoveUpRight className="hover:text-orange-500 transition duration-500"/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className={`${courierPrime.className} border rounded-[5px] py-[2px] px-3 dark:bg-gray-200 bg-gray-500 text-black`}>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/* github project button */}
+                <Link href={project.github} target="_blank" rel="noopener noreferrer" className="" >
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[40px] h-[40px] dark:bg-gray-700 bg-gray-400 rounded-full flex justify-center  items-center group mt-[2px]">
+                        <Github className="hover:text-orange-500 transition duration-500"/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className={`${courierPrime.className} border rounded-[5px] py-[2px] px-3 dark:bg-gray-200 bg-gray-500 text-black`}>Github Repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                </div>
               </div>
             </div>
             <div className="w-full">slider</div>
