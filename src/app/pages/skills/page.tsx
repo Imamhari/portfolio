c49f229a1c623b43a5384d1cd239cca48612dddb
Hari <imamhari3001@gmail.com>
2025-02-15 13:15:07 +0700

    project 80%

--- src/app/pages/skills/page.tsx
+++ src/app/pages/skills/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Skills() {
   return (
-    <section id="skills">Skills</section>
+    <section id="skills">
+      <Image
+                    src="/profile-light.png"
+                    width={300}
+                    height={30}
+                    alt="profile picture"
+                    priority
+                    className="block dark:hidden"
+                  />
+    
+    </section>
   )
 }
 
