d29d3fabccfb47d61c462cb8006cdc52432dcee6
Imam Hari <imamhari3001@gmail.com>
2025-04-14 23:46:38 +0700

    menambahkan icons di local

--- src/app/components/Skill.tsx
+++ src/app/components/Skill.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
 const icons = [
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/html-5--v1.png" alt="html-5--v1"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/css3.png" alt="css3"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/javascript--v1.png" alt="javascript--v1"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/typescript.png" alt="typescript"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/plasticine/100/react.png" alt="react"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/nextjs.png" alt="nextjs"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/bootstrap--v2.png" alt="bootstrap--v2"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/tailwindcss.png" alt="tailwindcss"/>},
+  {icon : <img src="/assets/icons/icons8-html-5.svg" alt='html-5'/>},
+  {icon : <img src="/assets/icons/icons8-css3.svg" alt='css-3'/>},
+  {icon : <img src="/assets/icons/icons8-javascript.svg" alt='javascript'/>},
+  {icon : <img src="/assets/icons/icons8-typescript.svg" alt='typescript'/>},
+  {icon : <img src="/assets/icons/icons8-react.svg" alt='react'/>},
+  {icon : <img src="/assets/icons/icons8-nextjs.svg" alt='nextjs'/>},
+  {icon : <img src="/assets/icons/icons8-bootstrap.svg" alt='bootstrap'/>},
+  {icon : <img src="/assets/icons/icons8-tailwindcss.svg" alt='tailwind-css'/>},
 ]
 
 function Skill() {
   return (
     <div className="flex flex-wrap items-center justify-center gap-2 xl:mx-5">
       {icons.map((item,index) => (
-        <div key={index} className="flex items-center justify-center w-[100px] h-[100px] bg-[#3f4346] rounded-lg shadow-md text-white max-w-[100px] hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer m-2">
+        <div key={index} style={{ width: '100px', height: '100px', backgroundColor: '#3f4346' }} className="flex items-center justify-center rounded-lg shadow-md text-white max-w-[100px] hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer m-2">
           <div>{item.icon}</div>
         </div>
       ))}
--- src/app/components/Tools.tsx
+++ src/app/components/Tools.tsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const icons = [
-  {icon : <img width="70" height="70" src="https://img.icons8.com/ios-filled/50/228BE6/visual-studio.png" alt="visual-studio"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/fluency/48/github.png" alt="github"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/git.png" alt="git"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/ios-filled/50/command-line.png" alt="command-line"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/npm.png" alt="npm"/>},
-  {icon : <img width="70" height="70" src="https://img.icons8.com/color/48/trello.png" alt="trello"/>},
-]
+  {icon : <img src="/assets/icons/icons8-visual-studio.svg" alt='visual-studio'/>},
+  {icon : <img src="/assets/icons/icons8-github.svg" alt='github'/>},
+  {icon : <img src="/assets/icons/icons8-git.svg" alt='git'/>},
+  {icon : <img src="/assets/icons/icons8-command-line-50.png" alt='command-line'/>},
+  {icon : <img src="/assets/icons/icons8-npm.svg" alt='npm'/>},
+  {icon : <img src="/assets/icons/icons8-trello.svg" alt='trello'/>},
+  {icon : <img src="/assets/icons/icons8-postman-inc-70.png" alt='postman'/>},
+  {icon : <img src="/assets/icons/icons8-figma.svg" alt='figma'/>},
+  
+];
 
 function Tools() {
   return (
     <div className="flex flex-wrap items-center justify-center gap-2 xl:mx-5">
-      {icons.map((item,index) => (
-        <div key={index} className="flex items-center justify-center w-[100px] h-[100px] bg-[#3f4346] rounded-lg shadow-md text-white max-w-[100px] hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer m-2">
+      {icons.map((item, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-center w-[100px] h-[100px] bg-[#3f4346] rounded-lg shadow-md text-white max-w-[100px] hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer m-2"
+        >
           <div>{item.icon}</div>
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Tools
+export default Tools;
