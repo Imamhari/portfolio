f09698b72e86406f92fc77bfab8c150a34ca0bc3
Imam Hari <imamhari3001@gmail.com>
2025-04-16 09:08:36 +0700

    menghapus file HeroSection

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,8 +1,30 @@
-import React from "react";
-import HeroSection from "@/app/components/HeroSection";
-import dynamic from 'next/dynamic';
+"use client";
+import dynamic from "next/dynamic";
+import {
+  IBM_Plex_Mono,
+  Courier_Prime,
+  Source_Code_Pro,
+} from "next/font/google";
+import { Typewriter } from "react-simple-typewriter";
+import DownloadCv from "@/app/components/atoms/DownloadCv";
+import SocialMedia from "@/app/components/atoms/SocialMedia";
+import StatsCard from "@/app/components/atoms/StatsCard";
 
-const Lanyard = dynamic(() => import('@/app/components/Lanyard/Lanyard'), {
+const ibmPlexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+const courierPrime = Courier_Prime({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const sourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const Lanyard = dynamic(() => import("@/app/components/Lanyard/Lanyard"), {
   ssr: false,
   loading: () => <div></div>,
 });
@@ -13,19 +35,57 @@ function Home() {
       id="home"
       className="relative w-full min-h-screen overflow-hidden flex flex-col-reverse lg:flex-row items-center justify-between"
     >
-
       {/* Konten Halaman */}
-     
-        <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-screen relative">
-          <div className=" w-full h-screen xl:w-auto items-center">
-            <Lanyard position={[0, 0, 13]} gravity={[0, -40, 0]} />
-          </div>
-            <div>
 
-            <HeroSection />
-            </div>
-          
+      <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-screen relative">
+        <div className=" w-full h-screen xl:w-auto items-center">
+          <Lanyard position={[0, 0, 13]} gravity={[0, -40, 0]} />
         </div>
+        <div className="flex flex-col md:pt-[70px] xl:pt-[150px] text-black dark:text-white relative lg:mr-[10vh]">
+          <div className="flex flex-col gap-6 xl:max-w-2xl w-full mx-auto  md:justify-center md:items-start ">
+            <h1
+              className={`${ibmPlexMono.className} text-4xl xl:text-5xl  font-bold leading-tight text-center md:text-left`}
+            >
+              Hey, I&apos;m <span className="text-orange-500">Imam Hari</span>
+            </h1>
+
+            <h2
+              className={`${courierPrime.className} text-lg md:text-3xl font-medium text-gray-700 dark:text-gray-300  text-left`}
+            >
+              I&apos;m a{" "}
+              <span className="text-orange-600 font-semibold">
+                <Typewriter
+                  words={[
+                    "Frontend Developer",
+                    "Web Developer",
+                    "Software Engineer",
+                    "Tech Enthusiast",
+                  ]}
+                  loop={0}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={150}
+                  deleteSpeed={150}
+                  delaySpeed={2000}
+                />
+              </span>
+            </h2>
+            <p
+              className={`${sourceCodePro.className} text-base md:text-lg text-black dark:text-white text-justify mx-auto xl:mx-0  font-bold`}
+            >
+              &quot;I don’t just write code, I build interactive and efficient
+              digital experiences. With a passion for innovation, I create
+              modern, responsive interfaces focused on user comfort and seamless
+              functionality.&quot;
+            </p>
+            <div className="flex flex-col space-y-3 xl:items-start justify-center items-center w-full">
+              <SocialMedia />
+              <DownloadCv />
+              <StatsCard />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/app/pages/projects/page.tsx
+++ src/app/pages/projects/page.tsx
@@ -222,7 +222,7 @@ function Projects() {
               {projectList.map((item, index) => {
                 return (
                   <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-gray-300">
+                    <div className="h-[460px] relative group flex justify-center items-center border-2 rounded-lg overflow-hidden">
                       {/* overlay */}
                       <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       {/* image */}
